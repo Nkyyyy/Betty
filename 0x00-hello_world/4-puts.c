612e7998fa94275fcc610f909081b3e98cc47d8b
Nkyyyy <odinakafv14423@gmail.com>
2023-07-13 03:37:22 -0700

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - Print output using puts
+ * main - Print output using puts
  *
- *Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  *
  */
 int main(void)
