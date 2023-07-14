031fc35a715c77fe44d83f20115b385cc895e5d3
Nkyyyy <odinakafv14423@gmail.com>
2023-07-14 06:34:36 -0700

    update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,12 @@ int main(void)
 	char alphabet = 'a';
 
 	while (alphabet <= 'z')
-
+	{
 		if (alphabet != 'q' && alphabet != 'e')
 		{
 		putchar(alphabet);
 		}
+	}
 	alphabet++;
 	putchar('\n');
 	return (0);
