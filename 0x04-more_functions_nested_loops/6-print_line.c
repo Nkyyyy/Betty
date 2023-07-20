b99351603f7efd6c5826135b4c3f0b8e9ca41993
Nkyyyy <odinakafv14423@gmail.com>
2023-07-20 11:02:20 -0700

    print a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - print a straight line
+ * @n: number of times the character _ should be printed
+ * Return: nothing
+ *
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (n > 0)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
+
