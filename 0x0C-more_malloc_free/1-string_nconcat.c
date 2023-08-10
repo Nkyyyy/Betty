c31ea1ab615bd32119e49d376e227fc182be49e3
Nkyyyy <odinakafv14423@gmail.com>
2023-08-10 23:30:39 +0000

    catenantion of strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *s3 = 0;
-	unsigned int len1, len2, a, b = 0;
+	unsigned int len1 = 0, len2 = 0, a = 0, b = 0;
 
 	while (s1 && s1[len1])
 		len1++;
