997a1f7f9325314465024de7736bdde7df3f5aa2
Nkyyyy <odinakafv14423@gmail.com>
2023-08-24 22:38:41 +0000

    add a new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,7 +13,7 @@ list_t *add_node(list_t **head, const char *str)
 	struc = malloc(sizeof(list_t));
 	if (struc == NULL)
 		return (NULL);
-	for (len = 0; str[len] != '\0'; len++)
+	for (len = 0; str[len]; len++)
 
 	struc->str = strdup(str);
 	struc->len = len;
