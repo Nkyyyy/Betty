6c74d4c57d1ce15cc87a02867258665349d0690f
Nkyyyy <odinakafv14423@gmail.com>
2023-08-24 23:51:51 +0000

    first print to the console

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * first - takes no values or arguement
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
