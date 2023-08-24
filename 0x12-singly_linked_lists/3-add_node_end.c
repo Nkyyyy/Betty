0ff2e0788308f5757c89db72e966b14763a2280a
Nkyyyy <odinakafv14423@gmail.com>
2023-08-24 23:33:28 +0000

    at the end of the node

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = len;
 	new->next = NULL;
-	if (!new) 
+	if (!new)
 		return (NULL);
 
 	if (*head == NULL)
